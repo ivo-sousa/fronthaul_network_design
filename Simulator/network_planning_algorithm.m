3b03856f047f0f8ccd1e6dea65b9bfb8a0c98ca2
Ivo Sousa <35538790+ivo-sousa@users.noreply.github.com>
2020-06-03 20:16:05 +0100

    Add files via upload

--- Simulator/network_planning_algorithm.m
+++ Simulator/network_planning_algorithm.m
@@ -22,10 +22,22 @@ nr_points=size(RRH_info,1);
 
 %BBU_capacity,BBU_cost,min_BBUs,max_BBUs
 BBU_info=readtable('BBU.dat');
-BBU_capacity=table2array(BBU_info(1,1)); % Max BBU capacity in Mbps
-BBU_cost=table2array(BBU_info(1,2)); % BBU cost in Euro
-min_BBUs=table2array(BBU_info(1,3)); % minimum no. of BBUs to test
-max_BBUs=table2array(BBU_info(1,4)); % maximum no. of BBUs to test
+RRHs_max=table2array(BBU_info(1,1)); % Max RRHs supported by a BBU
+link_capacity=table2array(BBU_info(1,2)); % Max capacity of each RRH-BBU link in Mbps
+BBU_cost=table2array(BBU_info(1,3)); % BBU cost in Euro
+min_BBUs=table2array(BBU_info(1,4)); % minimum no. of BBUs to test
+max_BBUs=table2array(BBU_info(1,5)); % maximum no. of BBUs to test
+
+debits=table2array(RRH_info(:,3));
+max_debit_link=max(debits);
+if max_debit_link > link_capacity
+    disp('BBUs cannot support the required bit rate of some RRH-BBU links!');
+    pos_BBUs=-1;
+    idx_RRH_BBU=-1;
+    RRHs_eq_ID=-1;
+    network_cost=inf;
+    return
+end
 
 if max_BBUs > nr_points
     max_BBUs=nr_points;
@@ -44,12 +56,14 @@ for sim_it=1:total_sim
     i_BBUs=sim_it+min_BBUs-1;
     
     [pos_BBUs_aux, idx_RRH_BBU_aux, RRHs_eq_ID_aux, network_cost_aux] = ...
-        network_planning_algorithm_aux(RRH_info,nr_iterations,BBU_capacity,BBU_cost,i_BBUs);
-    if pos_BBUs_aux ~= -1
-        pos_BBUs_sim(sim_it)={pos_BBUs_aux};
-        idx_RRH_BBU_sim(:,sim_it)=idx_RRH_BBU_aux;
-        RRHs_eq_ID_sim(:,sim_it)=RRHs_eq_ID_aux;
-        network_cost_sim(sim_it)=network_cost_aux;
+        network_planning_algorithm_aux(RRH_info,nr_iterations,RRHs_max,BBU_cost,i_BBUs);
+    if pos_BBUs_aux ~= -1 
+        if network_cost_aux ~= -2
+            pos_BBUs_sim(sim_it)={pos_BBUs_aux};
+            idx_RRH_BBU_sim(:,sim_it)=idx_RRH_BBU_aux;
+            RRHs_eq_ID_sim(:,sim_it)=RRHs_eq_ID_aux;
+            network_cost_sim(sim_it)=network_cost_aux;
+        end
     end
     
 end
@@ -59,7 +73,15 @@ if pos_BBUs_aux == -1 % more BBUs are required
     pos_BBUs=-1;
     idx_RRH_BBU=-1;
     RRHs_eq_ID=-1;
-    network_cost=-1;
+    network_cost=inf;
+elseif network_cost_aux == -2 % either add more BBUs or change RRHs assignment
+    disp('Maximum RRHs per BBU exceeded!');
+    disp('Either add more BBUs or change RRHs assignment!');
+    disp('("pos_BBUs" & "idx_RRH_BBU" can be used as starting points)');
+    pos_BBUs=pos_BBUs_aux;
+    idx_RRH_BBU=idx_RRH_BBU_aux;
+    RRHs_eq_ID=-2;
+    network_cost=inf;
 else
     [network_cost,best_choice]=min(network_cost_sim);
     RRHs_eq_ID=RRHs_eq_ID_sim(:,best_choice);
@@ -71,7 +93,7 @@ end
 end
 
 
-function [pos_BBUs, idx_RRH_BBU, RRHs_eq_ID, network_cost] = network_planning_algorithm_aux(RRH_info,nr_iterations,BBU_capacity,BBU_cost,i_BBUs)
+function [pos_BBUs, idx_RRH_BBU, RRHs_eq_ID, network_cost] = network_planning_algorithm_aux(RRH_info,nr_iterations,RRHs_max,BBU_cost,i_BBUs)
 
 %%%%% Other Inputs
 % RRH_info=readtable('RRH.dat');
@@ -87,10 +109,8 @@ nr_points=size(RRH_info,1);
 points=table2array(RRH_info(:,[1 2]));
 debits=table2array(RRH_info(:,3));
 
-req_capacity=sum(debits);
 
-k_min=ceil(req_capacity/BBU_capacity);
-if k_min > i_BBUs % not possible
+if nr_points > (i_BBUs*RRHs_max) % if more RRHs than the ones supported by BBUs
     pos_BBUs=-1;
     idx_RRH_BBU=-1;
     RRHs_eq_ID=-1;
@@ -98,6 +118,20 @@ if k_min > i_BBUs % not possible
     
     return
 end
+
+% req_capacity=sum(debits);
+% 
+% k_min=ceil(req_capacity/BBU_capacity);
+% if k_min > i_BBUs % not possible
+%     pos_BBUs=-1;
+%     idx_RRH_BBU=-1;
+%     RRHs_eq_ID=-1;
+%     network_cost=inf;
+%     
+%     return
+% end
+
+
 
 k_test=i_BBUs;
 
@@ -122,18 +156,22 @@ for j=1:nr_iterations
     D=sqrt(D)/1000;
     
     k_aux=1;
-    debit_ok=1;
+    RRHs_ok=1;
     total_cost=k_test*BBU_cost;
     
-    while k_aux<=k_test && debit_ok==1
+    while k_aux<=k_test && RRHs_ok==1
         idx_test= idx==k_aux;
         debits_test=debits(idx_test);
-        test_debit=sum(debits_test);
-        if test_debit>BBU_capacity          %associated RRHs demand more throughput than a BBU can handle
-            debit_ok=0;
+        %test_debit=sum(debits_test);
+        n_test=sum(idx_test);
+        %if test_debit>BBU_capacity          %associated RRHs demand more throughput than a BBU can handle
+        if n_test > RRHs_max % if assigned RRHs are more than the ones supported by a BBU
+            RRHs_ok=0;
+            pos_BBUs_sug=C;
+            idx_RRH_BBU_sug=idx;
         else
             D_test=D(idx_test,k_aux);
-            n_test=sum(idx_test);
+            %n_test=sum(idx_test);
             eq_ID_aux=cell(n_test,1);
             for dots=1:n_test
                 [cost,eq_ID]=link_design_algorithm(D_test(dots),debits_test(dots));
@@ -147,7 +185,7 @@ for j=1:nr_iterations
             k_aux=k_aux+1;
         end
     end
-    if debit_ok == 1
+    if RRHs_ok == 1
         sim_ok=1;
         
         network_cost_temp(j)=total_cost;
@@ -165,11 +203,11 @@ if sim_ok==1
     RRHs_eq_ID=RRHs_eq_ID_temp(:,best_choice);
     idx_RRH_BBU=idx_RRH_BBU_temp(:,best_choice);
     pos_BBUs=pos_BBUs_temp(:,:,best_choice);
-else
-    pos_BBUs=-1;
-    idx_RRH_BBU=-1;
-    RRHs_eq_ID=-1;
-    network_cost=inf;
+else % either add more BBUs or change RRHs assignment
+    pos_BBUs=pos_BBUs_sug;
+    idx_RRH_BBU=idx_RRH_BBU_sug;
+    RRHs_eq_ID=-2;
+    network_cost=-2;
 end
 end
 
